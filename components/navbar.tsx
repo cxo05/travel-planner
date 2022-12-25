60f58f4ebbfc5311dfe2c85c60c5bd5c03d7a6f0
Xavier <xavieroh05@gmail.com>
2022-12-25 14:55:19 +0800

    Add nextauth for google sign in

---
+++ components/navbar.tsx
@@ -0,0 +1,24 @@
+import styles from '../styles/Home.module.css'
+
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Navbar() {
+  const { data: session, status } = useSession()
+  const userEmail = session?.user?.email
+
+  return (
+    <nav className={styles.navbar}>
+      {status === "authenticated" ? (
+        <p>Signed in as {userEmail}</p>
+      ) : (<></>)
+      }
+      <div className={styles.signup}>
+        {status === "authenticated" ? (
+          <button onClick={() => signOut()}>Sign out</button>
+        ) : (
+          <button onClick={() => signIn()}>Sign in</button>
+        )}
+      </div>
+    </nav>
+  );
+};
