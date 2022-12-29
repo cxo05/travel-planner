bb592d9b238ff9073dffc2578b43cef7672532d7
Xavier <xavieroh05@gmail.com>
2022-12-29 14:55:35 +0800

    Update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,17 +7,19 @@ export default function Navbar() {
   const userEmail = session?.user?.email
 
   return (
-    <nav className="flex gap-4">
-      {status === "authenticated" ? (
-        <p>Signed in as {userEmail}</p>
-      ) : (<></>)
-      }
-      <div className={styles.signup}>
-        {status === "authenticated" ? (
-          <button onClick={() => signOut()}>Sign out</button>
-        ) : (
-          <button onClick={() => signIn()}>Sign in</button>
-        )}
+    <nav className="flex flex-col justify-center overflow-hidden bg-gray-50">
+      <div className="flex items-center justify-between border-b bg-blue-400 p-3">
+        <div className="text-lg font-bold text-gray-100">Travel Planner</div>
+        <div className="flex items-center space-x-5 text-gray-100">
+          {status === "authenticated" && <p>Signed in as {userEmail}</p>}
+          <div className={styles.signup}>
+            {status === "authenticated" ? (
+              <button onClick={() => signOut()} className="rounded bg-gray-100 py-1 px-2 text-slate-500 shadow-md">Sign out</button>
+            ) : (
+              <button onClick={() => signIn()} className="rounded bg-gray-100 py-1 px-2 text-slate-500 shadow-md">Sign in</button>
+            )}
+          </div>
+        </div>
       </div>
     </nav>
   );
