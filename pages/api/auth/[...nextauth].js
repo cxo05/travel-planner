6d609cf4ee82a5783c93e9fed67379f1571a4c21
Xavier <xavieroh05@gmail.com>
2022-12-29 14:56:11 +0800

    Misc

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,4 +13,10 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
 })
